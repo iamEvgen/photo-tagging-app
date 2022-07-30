20889644cc66c06aebf8151d6eb919c2e97d378f
iamEvgen <evgen.alexa@gmail.com>
2022-07-30 14:25:39 +0300

    add leaderboard

--- src/components/NavHome.js
+++ src/components/NavHome.js
@@ -4,10 +4,19 @@ import './navhome.css';
 
 function NavHome() {
   const ratingIcon = require('../images/rating.png');
+  const homeIcon = require('../images/home.png');
 
   return (
     <div className="navHome">
       <div className="navhome--logo">Photo tagging app</div>
+      <Link className="nav-link" to={'/'}>
+        <img
+          className="nav-icon navgame--home"
+          src={homeIcon}
+          alt="rating icon"
+        />
+        Home
+      </Link>
       <Link className="nav-link" to={`/leaderboard`}>
         <img
           className="nav-icon"
--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,20 +1,113 @@
 import React from 'react';
 import { getPlayers } from '../utils/firebase';
+import NavHome from '../components/NavHome';
+import './leaderboard.css';
+import uniqid from 'uniqid';
 
 function Leaderboard() {
-  const [results, setResults] = React.useState([]);
+  // const [results, setResults] = React.useState([]);
+  const [splitResults, setSplitResults] = React.useState([]);
+  const [selectedGame, setSelectedGame] = React.useState(0);
 
-  function getResults() {
-    getPlayers().then((res) => setResults(res));
+  React.useEffect(() => {
+    getPlayers().then((res) => {
+      // setResults(res);
+      setSplitResults([
+        res.filter((result) => result.gameId === '0'),
+        res.filter((result) => result.gameId === '1'),
+        res.filter((result) => result.gameId === '2'),
+      ]);
+    });
+  }, []);
 
-    setTimeout(() => {
-      console.log(results);
-    }, 2000);
+  const gamePoster1 = require(`../images/games/robot-city-main.jpeg`);
+  const gamePoster2 = require(`../images/games/underground-main.jpeg`);
+  const gamePoster3 = require(`../images/games/cyberpunk-main.jpeg`);
+
+  function handleGameIconClick(number) {
+    setSelectedGame(number);
+  }
+
+  const selectedGameStyle = { boxShadow: '0px 0px 10px 5px white' };
+
+  let rows;
+  if (splitResults.length > 0) {
+    rows = splitResults[selectedGame]
+      .sort((a, b) => {
+        if (a.time > b.time) {
+          return 1;
+        } else if (a.time < b.time) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+      .slice(0, 20)
+      .map((result) => {
+        return (
+          <tr key={uniqid()}>
+            <td className="table-data">{result.name}</td>
+            <td className="table-data">{result.time}</td>
+            <td className="table-data">{result.date}</td>
+          </tr>
+        );
+      });
   }
 
   return (
     <div>
-      <button onClick={getResults}>print players</button>
+      <NavHome />
+      <div className="leaderboard--content">
+        <div className="leaderboard--games">
+          <div
+            style={selectedGame === 0 ? selectedGameStyle : {}}
+            className="leaderboard--image-container"
+            onClick={() => handleGameIconClick(0)}
+          >
+            <img
+              className="leaderboard--image"
+              src={gamePoster1}
+              alt="robot city poster"
+            />
+            <div className="home--text-on-image">Robo City</div>
+          </div>
+          <div
+            style={selectedGame === 1 ? selectedGameStyle : {}}
+            className="leaderboard--image-container"
+            onClick={() => handleGameIconClick(1)}
+          >
+            <img
+              className="leaderboard--image"
+              src={gamePoster2}
+              alt="robot city poster"
+            />
+            <div className="home--text-on-image">Universe 113</div>
+          </div>
+          <div
+            style={selectedGame === 2 ? selectedGameStyle : {}}
+            className="leaderboard--image-container"
+            onClick={() => handleGameIconClick(2)}
+          >
+            <img
+              className="leaderboard--image"
+              src={gamePoster3}
+              alt="robot city poster"
+            />
+            <div className="home--text-on-image">Cyberpunk</div>
+          </div>
+        </div>
+
+        <table className="leaderboard--table">
+          <tbody>
+            <tr>
+              <th className="table-data">Name</th>
+              <th className="table-data">Time</th>
+              <th className="table-data">Date</th>
+            </tr>
+            {rows && rows}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
